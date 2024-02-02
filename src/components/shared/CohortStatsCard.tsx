0a715f8e08585b9b41227e1d0a6334660d559b4a
renzo <renzobocanegra.a@gmail.com>
2024-02-01 20:29:36 -0500

    feat: add cohort stats card

---
+++ src/components/shared/CohortStatsCard.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { FC } from "react";
+import BarsChart from "./charts/BarsChart";
+
+// pending: replace for db fetching
+const cohorts = {
+  totalCohorts: 40,
+  chartData: {
+    labels: ["prev years", "2020", "2021", "2022", "2023"],
+    data: [80, 165, 198, 190, 230],
+  },
+};
+
+const CohortStatsCard: FC = () => {
+  return (
+    <div className="w-96 space-y-2 rounded-lg bg-background px-4 py-3 shadow">
+      <div className="flex flex-row items-center justify-between">
+        <p className="font-bold capitalize">Cohorts Summary</p>
+        <Link href="/admin/cohorts" className="text-xs text-tertiary">
+          More Details
+        </Link>
+      </div>
+      <div className="-mb-2 flex flex-col items-start justify-start">
+        <p className="text-2xl font-black text-tertiary">
+          {cohorts.chartData.data.reduce((a, b) => b + a)}
+        </p>
+      </div>
+      <BarsChart chartData={cohorts.chartData} />
+    </div>
+  );
+};
+
+export default CohortStatsCard;
---
+++ src/components/shared/charts/BarsChart.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { chartColors, chartColorsSoft } from "@/components/dashboard/constants";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  Legend,
+  LinearScale,
+  Tooltip,
+} from "chart.js";
+import type { ChartData, ChartOptions } from "chart.js";
+import { FC } from "react";
+import { Bar } from "react-chartjs-2";
+
+interface BarsChartProps {
+  chartData: {
+    labels: string[];
+    data: number[];
+  };
+}
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
+
+const BarsChart: FC<BarsChartProps> = ({ chartData }) => {
+  const options: ChartOptions<"bar"> = {
+    responsive: true,
+    scales: {
+      x: {
+        display: true,
+        grid: {
+          display: false,
+        },
+        ticks: {
+          color: "#9699A6",
+        },
+        border: {
+          display: false,
+        },
+      },
+      y: {
+        display: true,
+        grid: {
+          display: false,
+        },
+        ticks: {
+          color: "#9699A6",
+        },
+        border: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      title: {
+        display: false,
+      },
+      tooltip: {
+        backgroundColor: "rgb(0, 0, 0)",
+        titleAlign: "center",
+        caretSize: 0,
+        padding: 8,
+        titleFont: { size: 14 },
+        bodyFont: { size: 14 },
+      },
+    },
+  };
+
+  const data: ChartData<"bar"> = {
+    labels: chartData.labels,
+    datasets: [
+      {
+        data: chartData.data,
+        backgroundColor: chartColorsSoft[0],
+        hoverBackgroundColor: chartColors[0],
+        borderRadius: 5,
+        borderSkipped: false,
+      },
+    ],
+  };
+
+  return <Bar options={options} data={data} className="p-2" />;
+};
+
+export default BarsChart;
