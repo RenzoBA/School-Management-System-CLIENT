7729e55788c01daab04ce8b8a5a4b23895c3047a
renzo <renzobocanegra.a@gmail.com>
2024-02-01 19:41:01 -0500

    feat: add chartColorsSoft

--- src/components/dashboard/constants/index.ts
+++ src/components/dashboard/constants/index.ts
@@ -121,3 +121,12 @@ export const chartColors: string[] = [
   "rgb(54, 162, 235)",
   "rgb(153, 102, 255)",
 ];
+
+export const chartColorsSoft: string[] = [
+  "rgba(75, 192, 192, 0.6)",
+  "rgba(255, 206, 86, 0.6)",
+  "rgba(255, 99, 132, 0.6)",
+  "rgba(255, 159, 64, 0.6)",
+  "rgba(54, 162, 235, 0.6)",
+  "rgba(153, 102, 255, 0.6)",
+];
