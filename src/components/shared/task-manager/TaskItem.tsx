b116efc5a5e3dc42370f2846125257ec6fa12e8b
John Patrick <jpattrick538@gmail.com>
2024-02-06 22:51:13 +0300

    feat: add task item

---
+++ src/components/shared/task-manager/TaskItem.tsx
@@ -0,0 +1,29 @@
+import { FC } from "react";
+import { TaskStatus } from "./TaskStatus";
+
+export type TaskItemProp = {
+    id: string;
+    title: string;
+    description: string;
+    authorId: string
+    assignedUserId?: string;
+    status: string;
+    dateCreated: string;
+}
+
+const TaskItem: FC<TaskItemProp> = ({ id, title, description, status, authorId, assignedUserId, dateCreated }) => {
+    return <div className="flex flex-col cursor-pointer px-2 py-1 rounded-sm hover:bg-background-strong gap-y-1 h-max ">
+        <div className="flex justify-center align-middle items-center ">
+            <h3 className="font-bold text-tertiary">{title}</h3>
+            <div className="ml-auto text-sm">
+                <TaskStatus
+                status={status}
+                />
+            </div>
+        </div>
+        <p className="truncate">{description}</p>
+        {/* <p>{status}</p> */}
+    </div>;
+};
+
+export default TaskItem;
